9258ddefbf3914bfc885ba59bda3a0dede4e17ad
Xavi Guardia <xguardia@sfy.com>
2023-11-08 17:11:11 +0100

    Initial commit

---
+++ src/components/TagAlternative/TagAlternative.stories.js
@@ -0,0 +1,14 @@
+import { TagAlternative } from ".";
+
+export default {
+  title: "Components/TagAlternative",
+  component: TagAlternative,
+};
+
+export const Default = {
+  args: {
+    className: {},
+    iconHeart: "https://c.animaapp.com/u8uczHJ8/img/icon-heart-2@2x.png",
+    bordervisibleBox: "https://c.animaapp.com/u8uczHJ8/img/bordervisible-box-1.svg",
+  },
+};
---
+++ src/screens/CardImgMDefaultScreen/CardImgMDefaultScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CardImgMDefault } from "../../components/CardImgMDefault";
+import "./style.css";
+
+export const CardImgMDefaultScreen = () => {
+  return (
+    <CardImgMDefault
+      className="card-img-m-default-instance"
+      overlapGroupClassName="card-img-m-default-2"
+      tagAlternativeBordervisibleBox="https://c.animaapp.com/u8uczHJ8/img/bordervisible-box.svg"
+      tagBodyClassName="card-img-m-default-3"
+    />
+  );
+};
